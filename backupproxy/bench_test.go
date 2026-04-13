108e7dac37200301f978f7189dc1fdde54d9cda4
Son Roy Almerol <sralmerol@sgl.com>
2026-04-13 18:33:13 -0400

    Minimize allocations on hot paths, add benchmarks
    Zero-alloc paths achieved:
    - EncodeBlobTo with pooled dst: 0 allocs (was 2)
    - DynamicIndexWriter.Csum() cached: 0 allocs (was 1 per call)
    - DecodeBlob uncompressed: 0 allocs (already was)
    
    Reduced-alloc paths:
    - EncodeBlobTo (nil dst): 1 alloc (was 2)
    - encodeChunkBlob: 1 alloc (was 2), uses pooled buffer + EncodeBlobTo
    - encodeChunkBlob compressed: 2 allocs (was 3), uses EncodeCompressedBlobTo
    - DynamicIndexWriter.Add: 1 alloc for 256 entries (was 3), pre-allocated cap=256
    - DynamicIndexWriter.Finish: 3 allocs (was 5), eliminated bytes.Buffer
    - addFileInfo: 2 allocs (was 3), hex.Encode into stack buffer
    - UploadArchive: hex.Encode into [64]byte buffer (was heap string)
    
    Benchmarks added:
    - datastore: EncodeBlob, EncodeBlobTo, EncodeBlobToPooled,
      EncodeCompressedBlob, DecodeBlob, DynamicIndexWriter Add/Finish/Csum,
      ReadDynamicIndex+ComputeCsum
    - backupproxy: encodeChunkBlob, addFileInfo, hex encoding, SHA256,
      LocalUploadArchive end-to-end

---
+++ backupproxy/bench_test.go
@@ -0,0 +1,124 @@
+package backupproxy
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"testing"
+
+	"github.com/pbs-plus/pxar/buzhash"
+	"github.com/pbs-plus/pxar/datastore"
+)
+
+func BenchmarkEncodeChunkBlob(b *testing.B) {
+	chunk := make([]byte, 4096)
+	for i := range chunk {
+		chunk[i] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := encodeChunkBlob(chunk, false)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeChunkBlobCompressed(b *testing.B) {
+	chunk := make([]byte, 4096)
+	for i := range chunk {
+		chunk[i] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := encodeChunkBlob(chunk, true)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkAddFileInfo(b *testing.B) {
+	var digest [32]byte
+	rand.Read(digest[:])
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		var files []datastore.FileInfo
+		addFileInfo(&files, "root.pxar.didx", 65536, digest)
+	}
+}
+
+func BenchmarkHexEncodeDigest(b *testing.B) {
+	var digest [32]byte
+	rand.Read(digest[:])
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_ = hex.EncodeToString(digest[:])
+	}
+}
+
+func BenchmarkHexEncodeDigestBuffer(b *testing.B) {
+	var digest [32]byte
+	rand.Read(digest[:])
+	var buf [64]byte
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		hex.Encode(buf[:], digest[:])
+		_ = string(buf[:])
+	}
+}
+
+func BenchmarkSHA256Chunk(b *testing.B) {
+	chunk := make([]byte, 4096)
+	for i := range chunk {
+		chunk[i] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_ = sha256.Sum256(chunk)
+	}
+}
+
+func BenchmarkLocalUploadArchive(b *testing.B) {
+	data := bytes.Repeat([]byte("benchmark data for chunking test "), 512)
+
+	cfg, _ := buzhash.NewConfig(4096)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		idx := datastore.NewDynamicIndexWriter(0)
+		chunker := buzhash.NewChunker(bytes.NewReader(data), cfg)
+		var totalOffset uint64
+		for {
+			chunk, err := chunker.Next()
+			if err != nil {
+				break
+			}
+			digest := sha256.Sum256(chunk)
+			totalOffset += uint64(len(chunk))
+			idx.Add(totalOffset, digest)
+		}
+		_, _ = idx.Finish()
+	}
+}
---
+++ datastore/bench_test.go
@@ -0,0 +1,208 @@
+package datastore
+
+import (
+	"testing"
+)
+
+func BenchmarkEncodeBlob(b *testing.B) {
+	data := make([]byte, 4096)
+	for i := range data {
+		data[i] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := EncodeBlob(data)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeBlobTo(b *testing.B) {
+	data := make([]byte, 4096)
+	for i := range data {
+		data[i] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := EncodeBlobTo(nil, data)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeBlobToPooled(b *testing.B) {
+	data := make([]byte, 4096)
+	for i := range data {
+		data[i] = byte(i)
+	}
+	dst := make([]byte, 0, BlobHeaderSize+len(data))
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := EncodeBlobTo(dst[:0], data)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkEncodeCompressedBlob(b *testing.B) {
+	data := make([]byte, 4096)
+	for i := range data {
+		data[i] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := EncodeCompressedBlob(data)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDecodeBlobUncompressed(b *testing.B) {
+	data := make([]byte, 4096)
+	for i := range data {
+		data[i] = byte(i)
+	}
+	blob, _ := EncodeBlob(data)
+	encoded := blob.Bytes()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := DecodeBlob(encoded)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDecodeBlobCompressed(b *testing.B) {
+	data := make([]byte, 4096)
+	for i := range data {
+		data[i] = byte(i)
+	}
+	blob, _ := EncodeCompressedBlob(data)
+	encoded := blob.Bytes()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := DecodeBlob(encoded)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDynamicIndexWriterAdd(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		w := NewDynamicIndexWriter(0)
+		var offset uint64
+		for j := 0; j < 256; j++ {
+			offset += 4096
+			var digest [32]byte
+			digest[0] = byte(j)
+			w.Add(offset, digest)
+		}
+	}
+}
+
+func BenchmarkDynamicIndexWriterFinish(b *testing.B) {
+	var entries [256][32]byte
+	for i := range entries {
+		entries[i][0] = byte(i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		w := NewDynamicIndexWriter(0)
+		var offset uint64
+		for j := 0; j < 256; j++ {
+			offset += 4096
+			w.Add(offset, entries[j])
+		}
+		_, _ = w.Finish()
+	}
+}
+
+func BenchmarkDynamicIndexWriterCsum(b *testing.B) {
+	w := NewDynamicIndexWriter(0)
+	var offset uint64
+	for i := 0; i < 256; i++ {
+		offset += 4096
+		var digest [32]byte
+		digest[0] = byte(i)
+		w.Add(offset, digest)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_ = w.Csum()
+	}
+}
+
+func BenchmarkDynamicIndexWriterCsumAfterFinish(b *testing.B) {
+	w := NewDynamicIndexWriter(0)
+	var offset uint64
+	for i := 0; i < 256; i++ {
+		offset += 4096
+		var digest [32]byte
+		digest[0] = byte(i)
+		w.Add(offset, digest)
+	}
+	_, _ = w.Finish()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_ = w.Csum()
+	}
+}
+
+func BenchmarkDynamicIndexReadAndComputeCsum(b *testing.B) {
+	w := NewDynamicIndexWriter(0)
+	var offset uint64
+	for i := 0; i < 256; i++ {
+		offset += 4096
+		var digest [32]byte
+		digest[0] = byte(i)
+		w.Add(offset, digest)
+	}
+	raw, _ := w.Finish()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		r, err := ReadDynamicIndex(raw)
+		if err != nil {
+			b.Fatal(err)
+		}
+		r.ComputeCsum()
+	}
+}
